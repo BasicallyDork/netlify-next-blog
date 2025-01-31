d40a756bbc600697a56282a658b50c7b9c47b132
BasicallyDork <shubhamcfc@gmail.com>
2025-01-31 06:21:07 +0000

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="px-5 py-3 bg-white shadow-sm font-work-sans border-t-2 text-center">
+        <p>&#169; Pride&Pen, All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
